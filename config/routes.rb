621e5e1835a5d389da0ec6516f5f4a5d2c41eae7
Paul Louyot <paul@Pauls-MacBook-Pro.local>
2024-11-22 17:10:36 +0100

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  root "welcome#index"
+
+  get :new_session, to: "welcome#new"
+  post :sign_in, to: "welcome#sign_in"
+  resources :books, only: [ :index, :create, :update, :new ] do
+    member do
+      post :claim
+      post :unclaim
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,7 +19,4 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
